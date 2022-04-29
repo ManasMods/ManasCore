31848fa788623b12784cd69a45edc06c4d3514cf
Christian Raoulis <craoulis@googlemail.com>
2022-04-29 15:31:24 +0200

    initial setup

---
+++ src/main/java/com/github/manasmods/manascore/ManasCore.java
@@ -0,0 +1,34 @@
+package com.github.manasmods.manascore;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod("ManasCore")
+public class ManasCore {
+    public static final String MOD_ID = "manascore";
+
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public ManasCore() {
+
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+}
