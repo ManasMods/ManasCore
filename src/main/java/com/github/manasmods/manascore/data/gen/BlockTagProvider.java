094ed0592c890e42d0ee4e831862851aba733c05
Christian Raoulis <craoulis@googlemail.com>
2022-05-07 00:11:06 +0200

    add helper methods to add tool compatibility in BlockTagProvider

--- src/main/java/com/github/manasmods/manascore/data/gen/BlockTagProvider.java
+++ src/main/java/com/github/manasmods/manascore/data/gen/BlockTagProvider.java
@@ -1,6 +1,8 @@
 package com.github.manasmods.manascore.data.gen;
 
 import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 
 public abstract class BlockTagProvider extends BlockTagsProvider {
@@ -14,4 +16,27 @@ public abstract class BlockTagProvider extends BlockTagsProvider {
     }
 
     protected abstract void generate();
+
+    protected void mineableWithAxe(Block... blocks) {
+        tag(BlockTags.MINEABLE_WITH_AXE).add(blocks);
+    }
+
+    protected void mineableWithHoe(Block... blocks) {
+        tag(BlockTags.MINEABLE_WITH_HOE).add(blocks);
+    }
+
+    protected void mineableWithPickaxe(Block... blocks) {
+        tag(BlockTags.MINEABLE_WITH_PICKAXE).add(blocks);
+    }
+
+    protected void mineableWithShovel(Block... blocks) {
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).add(blocks);
+    }
+
+    protected void mineableWithAllTools(Block... blocks) {
+        mineableWithAxe(blocks);
+        mineableWithHoe(blocks);
+        mineableWithPickaxe(blocks);
+        mineableWithShovel(blocks);
+    }
 }
