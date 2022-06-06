9aa8131b4fbf45cc4bfc27688acb8c2ee2ee4f5c
Christian Raoulis <craoulis@googlemail.com>
2022-06-06 11:21:50 +0200

    add better keybinding registration and handling

--- src/main/java/com/github/manasmods/manascore/ManasCore.java
+++ src/main/java/com/github/manasmods/manascore/ManasCore.java
@@ -1,5 +1,6 @@
 package com.github.manasmods.manascore;
 
+import lombok.Getter;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -7,7 +8,8 @@ import org.apache.logging.log4j.Logger;
 @Mod(ManasCore.MOD_ID)
 public class ManasCore {
     public static final String MOD_ID = "manascore";
-    private static final Logger LOGGER = LogManager.getLogger();
+    @Getter
+    private static final Logger logger = LogManager.getLogger();
 
     public ManasCore() {
     }
