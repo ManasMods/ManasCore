ed1dd54289ee6180680ccdee425e7ddb40292177
Christian Raoulis <craoulis@googlemail.com>
2022-10-09 16:39:02 +0200

    add some more documentation

--- src/main/java/com/github/manasmods/manascore/api/skills/event/SkillEvent.java
+++ src/main/java/com/github/manasmods/manascore/api/skills/event/SkillEvent.java
@@ -4,12 +4,14 @@ import com.github.manasmods.manascore.api.skills.ManasSkill;
 import com.github.manasmods.manascore.api.skills.ManasSkillInstance;
 import lombok.Getter;
 import net.minecraftforge.eventbus.api.Event;
+import org.jetbrains.annotations.ApiStatus;
 
 /**
  * Base class for all {@link Event}s that involve {@link ManasSkill}s
  *
  * @see UnlockSkillEvent
  */
+@ApiStatus.AvailableSince("0.0.0.26")
 public class SkillEvent extends Event {
     @Getter
     private final ManasSkillInstance skillInstance;
--- src/main/java/com/github/manasmods/manascore/api/skills/event/UnlockSkillEvent.java
+++ src/main/java/com/github/manasmods/manascore/api/skills/event/UnlockSkillEvent.java
@@ -5,12 +5,14 @@ import com.github.manasmods.manascore.api.skills.ManasSkillInstance;
 import lombok.Getter;
 import net.minecraft.world.entity.Entity;
 import net.minecraftforge.eventbus.api.Cancelable;
+import org.jetbrains.annotations.ApiStatus;
 
 /**
  * This Event is fired right before a {@link Entity} unlocks a new {@link ManasSkill}.
  * You can prevent an {@link Entity} from learning that {@link ManasSkill} by canceling the {@link UnlockSkillEvent}.
  * <p>
  */
+@ApiStatus.AvailableSince("0.0.0.26")
 @Cancelable
 public class UnlockSkillEvent extends SkillEvent {
     @Getter
