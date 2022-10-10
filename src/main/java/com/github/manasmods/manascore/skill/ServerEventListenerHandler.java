ad5d58030abe3741948bcb66fd1d2b62c7655d3e
Christian Raoulis <craoulis@googlemail.com>
2022-10-10 10:52:29 +0200

    cause a resync after skill livingHurtListener invocation

--- src/main/java/com/github/manasmods/manascore/skill/ServerEventListenerHandler.java
+++ src/main/java/com/github/manasmods/manascore/skill/ServerEventListenerHandler.java
@@ -3,7 +3,9 @@ package com.github.manasmods.manascore.skill;
 import com.github.manasmods.manascore.ManasCore;
 import com.github.manasmods.manascore.api.skills.ManasSkillInstance;
 import com.github.manasmods.manascore.api.skills.SkillAPI;
+import com.github.manasmods.manascore.api.skills.capability.SkillStorage;
 import com.github.manasmods.manascore.api.skills.listener.LivingHurtListener;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -12,13 +14,17 @@ import net.minecraftforge.fml.common.Mod;
 class ServerEventListenerHandler {
     @SubscribeEvent
     public static void livingHurtListener(final LivingHurtEvent e) {
+        final LivingEntity entity = e.getEntityLiving();
         // In this case, we only want this to happen on Server Side
-        if (e.getEntityLiving().getLevel().isClientSide()) return;
+        if (entity.getLevel().isClientSide()) return;
         // Get all listening skills and invoke their event consumer
-        for (ManasSkillInstance skillInstance : SkillAPI.getSkillsFrom(e.getEntity()).getLearnedSkills()) {
+        SkillStorage skillStorage = SkillAPI.getSkillsFrom(entity);
+        for (ManasSkillInstance skillInstance : skillStorage.getLearnedSkills()) {
             if (skillInstance.getSkill() instanceof LivingHurtListener listener) {
                 listener.onLivingHurt(skillInstance, e);
             }
         }
+        //Sync changed Skills
+        skillStorage.syncChanges();
     }
 }
