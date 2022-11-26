000e20b32fd605a122a58d465626e0057950a78f
Christian Raoulis <craoulis@googlemail.com>
2022-11-26 17:19:54 +0100

    add common item model annotations

--- src/main/java/com/github/manasmods/manascore/api/data/gen/GenerateModels.java
+++ src/main/java/com/github/manasmods/manascore/api/data/gen/GenerateModels.java
@@ -4,11 +4,14 @@
 
 package com.github.manasmods.manascore.api.data.gen;
 
+import org.jetbrains.annotations.ApiStatus.AvailableSince;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@AvailableSince("2.0.1.0")
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface GenerateModels {
