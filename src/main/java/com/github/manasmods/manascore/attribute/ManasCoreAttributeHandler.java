04664738d554d026382b9581b1a549ac4557cf4b
Minh <minheragon12345@gmail.com>
2023-10-01 15:49:14 +0700

    Removed over-boosted sprint jump and added fall damage reduction for Jump Power.

--- src/main/java/com/github/manasmods/manascore/attribute/ManasCoreAttributeHandler.java
+++ src/main/java/com/github/manasmods/manascore/attribute/ManasCoreAttributeHandler.java
@@ -6,7 +6,6 @@ package com.github.manasmods.manascore.attribute;
 
 import com.github.manasmods.manascore.ManasCore;
 import net.minecraft.core.BlockPos;
-import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
@@ -14,6 +13,7 @@ import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.event.entity.player.CriticalHitEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -42,11 +42,17 @@ public class ManasCoreAttributeHandler {
         //Apply velocity
         Vec3 vec3 = entity.getDeltaMovement();
         entity.setDeltaMovement(vec3.x, verticalVelocity, vec3.z);
-        if (entity.isSprinting()) {
-            float f = entity.getYRot() * ((float) Math.PI / 180F);
-            entity.setDeltaMovement(entity.getDeltaMovement().add(-Mth.sin(f) * 0.2F, 0.0D, Mth.cos(f) * 0.2F));
-        }
         entity.hasImpulse = true;
+    }
+
+    @SubscribeEvent
+    public static void modifyFallDamage(final LivingFallEvent e) {
+        AttributeInstance instance = e.getEntity().getAttribute(ManasCoreAttributes.JUMP_POWER.get());
+        if (instance == null) return;
+        if (instance.getValue() == 0.42) return;
+
+        double additionalJumpBlock = (instance.getValue() - 0.42) / 0.2;
+        e.setDistance((float) (e.getDistance() - additionalJumpBlock));
     }
 
     @SubscribeEvent(priority = EventPriority.HIGH)
