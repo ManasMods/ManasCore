e8a5d0648ff17db653e84eb40084dc63b1404971
Christian Raoulis <CRaoulis@googlemail.com>
2023-12-24 15:35:12 +0100

    fix import issues in neoforge

--- neoforge/src/main/java/com/github/manasmods/manascore/neoforge/ManasCoreNeoForge.java
+++ neoforge/src/main/java/com/github/manasmods/manascore/neoforge/ManasCoreNeoForge.java
@@ -1,15 +1,11 @@
 package com.github.manasmods.manascore.neoforge;
 
 import com.github.manasmods.manascore.ManasCore;
-import dev.architectury.platform.forge.EventBuses;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.fml.common.Mod;
 
 @Mod(ManasCore.MOD_ID)
 public class ManasCoreNeoForge {
     public ManasCoreNeoForge() {
-		// Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(ManasCore.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         ManasCore.init();
     }
 }
