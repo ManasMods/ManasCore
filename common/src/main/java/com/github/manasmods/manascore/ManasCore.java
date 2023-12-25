7c4a4d7dc32b7af8c5b7eeb893efda7ffd021d61
Christian Raoulis <CRaoulis@googlemail.com>
2023-12-25 11:43:23 +0100

    remove test code

--- common/src/main/java/com/github/manasmods/manascore/ManasCore.java
+++ common/src/main/java/com/github/manasmods/manascore/ManasCore.java
@@ -1,12 +1,6 @@
 package com.github.manasmods.manascore;
 
-import com.github.manasmods.manascore.api.storage.Storage;
-import com.github.manasmods.manascore.api.storage.StorageEvents;
 import com.github.manasmods.manascore.storage.StorageManager;
-import com.github.manasmods.manascore.storage.StorageManager.StorageKey;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,27 +10,5 @@ public class ManasCore {
 
     public static void init() {
         StorageManager.init();
-
-        StorageEvents.REGISTER_ENTITY_STORAGE.register(registry -> {
-            StorageKey<TestStorage> key = registry.register(new ResourceLocation(MOD_ID, "test"), TestStorage.class, entity -> true, TestStorage::new);
-
-        });
-    }
-
-    private static class TestStorage extends Storage {
-
-        TestStorage(Entity entity) {
-
-        }
-
-        @Override
-        public void save(CompoundTag data) {
-
-        }
-
-        @Override
-        public void load(CompoundTag data) {
-
-        }
     }
 }
