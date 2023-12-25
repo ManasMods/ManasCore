e0cddeeb0084de6729df44044ebbb477b37c5f76
Christian Raoulis <CRaoulis@googlemail.com>
2023-12-25 14:49:45 +0100

    add test mods for forge and neo

--- fabricTest/src/main/java/com/github/manasmods/testmod/TestMod.java
+++ fabricTest/src/main/java/com/github/manasmods/testmod/TestMod.java
@@ -1,11 +1,10 @@
 package com.github.manasmods.testmod;
 
-import com.github.manasmods.manascore.ManasCore;
 import net.fabricmc.api.ModInitializer;
 
 public class TestMod implements ModInitializer {
     @Override
     public void onInitialize() {
-        ManasCore.Logger.info("TestMod initialized!");
+
     }
 }
