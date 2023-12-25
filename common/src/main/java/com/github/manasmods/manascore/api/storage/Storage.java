ec60e9e66ac7ef3e20e2edc0cef1dde5e5974f1d
Christian Raoulis <CRaoulis@googlemail.com>
2023-12-25 11:59:04 +0100

    add update functions to Storage for efficient packet handling

--- common/src/main/java/com/github/manasmods/manascore/api/storage/Storage.java
+++ common/src/main/java/com/github/manasmods/manascore/api/storage/Storage.java
@@ -8,14 +8,37 @@ public abstract class Storage {
     /**
      * Used to save data to the entity.
      * Add all information to the given tag.
+     *
+     * @see Storage#load(CompoundTag)
      */
     public abstract void save(CompoundTag data);
 
     /**
      * Used to load data from the entity.
      * Read all information from the given tag.
+     *
+     * @see Storage#save(CompoundTag)
      */
     public abstract void load(CompoundTag data);
+
+    /**
+     * Used to create update packets.
+     * Override this method to optimize the packet data.
+     *
+     * @see #loadUpdate(CompoundTag)
+     */
+    public void saveOutdated(CompoundTag data) {
+        this.save(data);
+    }
+
+    /**
+     * Used to apply update packets.
+     *
+     * @see #saveOutdated(CompoundTag)
+     */
+    public void loadUpdate(CompoundTag data) {
+        this.load(data);
+    }
 
     /**
      * Used to mark the storage as dirty.
--- common/src/main/java/com/github/manasmods/manascore/storage/CombinedStorage.java
+++ common/src/main/java/com/github/manasmods/manascore/storage/CombinedStorage.java
@@ -6,6 +6,7 @@ import com.github.manasmods.manascore.core.injection.StorageHolder;
 import com.github.manasmods.manascore.storage.StorageManager.StorageType;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.Nullable;
 
@@ -60,6 +61,22 @@ public class CombinedStorage {
         return tag;
     }
 
+    public void handleUpdatePacket(CompoundTag tag) {
+        ListTag entriesTag = tag.getList("manascore_registry_storage", ListTag.TAG_COMPOUND);
+
+        for (Tag e : entriesTag) {
+            CompoundTag entryTag = (CompoundTag) e;
+            ResourceLocation id = new ResourceLocation(entryTag.getString("manascore_registry_storage_id"));
+            Storage storage = this.storages.get(id);
+            if (storage == null) {
+                ManasCore.Logger.warn("Failed to find storage for id {}. All information about this storage will be dropped!", id);
+                continue;
+            }
+
+            storage.loadUpdate(entryTag);
+        }
+    }
+
     public void add(ResourceLocation id, Storage storage) {
         this.storages.put(id, storage);
     }
@@ -67,5 +84,21 @@ public class CombinedStorage {
     @Nullable
     public Storage get(ResourceLocation id) {
         return this.storages.get(id);
+    }
+
+    public CompoundTag createUpdatePacket() {
+        CompoundTag tag = new CompoundTag();
+
+        ListTag entriesTag = new ListTag();
+        this.storages.forEach((id, storage) -> {
+            if (!storage.isDirty()) return;
+            CompoundTag entryTag = new CompoundTag();
+            entryTag.putString("manascore_registry_storage_id", id.toString());
+            storage.saveOutdated(entryTag);
+            entriesTag.add(entryTag);
+        });
+
+        tag.put("manascore_registry_storage", entriesTag);
+        return tag;
     }
 }
