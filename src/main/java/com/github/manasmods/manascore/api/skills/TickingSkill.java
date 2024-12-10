98cdec2ced23ac1cb1c1429988531ef0aa120720
Minh <minheragon12345@gmail.com>
2024-12-10 14:53:47 +0700

    Ticking Skill small change

--- src/main/java/com/github/manasmods/manascore/api/skills/TickingSkill.java
+++ src/main/java/com/github/manasmods/manascore/api/skills/TickingSkill.java
@@ -6,28 +6,31 @@ import net.minecraft.world.entity.LivingEntity;
 
 import java.util.Optional;
 
+@Getter
 public class TickingSkill {
     private int duration = 0;
-    @Getter
     private final ManasSkill skill;
     public TickingSkill(ManasSkill skill) {
         this.skill = skill;
     }
 
     public boolean tick(SkillStorage storage, LivingEntity entity) {
-        Optional<ManasSkillInstance> optional = storage.getSkill(skill);
+        Optional<ManasSkillInstance> optional = storage.getSkill(this.getSkill());
         if (optional.isEmpty()) return false;
 
         ManasSkillInstance instance = optional.get();
         if (reachedMaxDuration(instance, entity)) return false;
 
-        if (!instance.canInteractSkill(entity)) return false;
+        if (!instance.canInteractSkill(entity)) {
+            instance.onRelease(entity, this.getDuration());
+            return false;
+        }
         return instance.onHeld(entity, this.duration++);
     }
 
     public boolean reachedMaxDuration(ManasSkillInstance instance, LivingEntity entity) {
         int maxDuration = instance.getMaxHeldTime(entity);
         if (maxDuration == -1) return false;
-        return duration >= maxDuration;
+        return this.getDuration() >= maxDuration;
     }
 }
