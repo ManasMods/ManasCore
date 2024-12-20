de25b5cc0a1240469e26b4d03efb8c4973d14ae1
Minh <minheragon12345@gmail.com>
2024-12-21 02:24:14 +0700

    Crash with piercing arrows

--- src/main/java/com/github/manasmods/manascore/capability/skill/event/ServerEventListenerHandler.java
+++ src/main/java/com/github/manasmods/manascore/capability/skill/event/ServerEventListenerHandler.java
@@ -8,9 +8,11 @@ import com.github.manasmods.manascore.api.skills.capability.SkillStorage;
 import com.github.manasmods.manascore.api.skills.event.SkillDamageEvent;
 import com.github.manasmods.manascore.api.skills.event.UnlockSkillEvent;
 import com.google.common.collect.Multimap;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.ProjectileImpactEvent;
@@ -149,6 +151,10 @@ public class ServerEventListenerHandler {
             if (!skillInstance.canInteractSkill(living)) continue;
 
             skillInstance.onProjectileHit(living, e);
+            if (e.isCanceled() && e.getProjectile()  instanceof AbstractArrow arrow) {
+                if (arrow.piercingIgnoreEntityIds == null) arrow.piercingIgnoreEntityIds = new IntOpenHashSet(10);
+                arrow.piercingIgnoreEntityIds.add(living.getId());
+            }
         }
         skillStorage.syncChanges();
     }
